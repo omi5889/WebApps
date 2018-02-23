46fe2abec9ab9447dbd904789c0f9ce44cdbaa79
Omkar Gadgil <ogadgil@cisco.com>
2018-02-22 22:55:16 -0800

    Snippy Init Commit

---
+++ Snippy/src/main/java/com/ezeebot/snippy/SnippyApplication.java
@@ -0,0 +1,12 @@
+package com.ezeebot.snippy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnippyApplication.class, args);
+	}
+}
---
+++ Snippy/src/test/java/com/ezeebot/snippy/SnippyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ezeebot.snippy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SnippyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
