53a5ffa18accd1c27693eabfb95fb63f76c72959
Omkar Gadgil <ogadgil@cisco.com>
2018-03-15 16:24:16 -0700

    initial commit of alpha app

---
+++ AlphaApp/src/main/java/com/cisco/alphaApp/AlphaAppApplication.java
@@ -0,0 +1,12 @@
+package com.cisco.alphaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaAppApplication.class, args);
+	}
+}
---
+++ AlphaApp/src/main/java/com/cisco/alphaApp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cisco.alphaApp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AlphaAppApplication.class);
+	}
+
+}
